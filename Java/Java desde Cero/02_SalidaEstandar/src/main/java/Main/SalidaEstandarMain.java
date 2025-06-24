c60bc5dcb537aafa75117ba78db88868a5fd6e06
ludwinolivarest <ludwin.olivares@gmail.com>
2025-06-24 16:15:09 -0500

    Salida estandar

---
+++ Java/Java desde Cero/02_SalidaEstandar/src/main/java/Main/SalidaEstandarMain.java
@@ -0,0 +1,9 @@
+
+package Main;
+
+public class SalidaEstandarMain {
+    public static void main(String args[]){
+        System.out.println("Esta es la salida de mi programa.");
+        System.out.println("Segunda linea de texto.");
+    }
+}
