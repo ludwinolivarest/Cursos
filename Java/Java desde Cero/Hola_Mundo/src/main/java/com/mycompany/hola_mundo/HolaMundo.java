75978f773c75ee87f0902a78a43be3ff377321ba
ludwinolivarest <ludwin.olivares@gmail.com>
2025-06-24 14:41:25 -0500

    Primer proyecto 'Hola Mundo'

---
+++ Java/Java desde Cero/Hola_Mundo/src/main/java/com/mycompany/hola_mundo/HolaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hola_mundo;
+
+/**
+ *
+ * @author AMBULANCIA
+ */
+public class HolaMundo {
+    public static void Main(String args[]){
+        System.out.println("Hola mundo");
+    }
+}
---
+++ Java/Java desde Cero/Hola_Mundo/src/main/java/com/mycompany/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola_mundo;
+
+/**
+ *
+ * @author AMBULANCIA
+ */
+public class Hola_Mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
